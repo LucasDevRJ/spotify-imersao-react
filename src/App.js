830d013c8310fc2697f7c4813004bc7fd3f31352
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-02-03 10:46:22 -0300

    Adição do Sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import Sidebar from './Sidebar/Sidebar';
 
 function App() {
   return (
     <div>
         <Header/>
+        <Sidebar/>
         <Footer/>
     </div>
   );
