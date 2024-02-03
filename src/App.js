79681b1cd4f75512988a332411cef58b684c2cbf
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-02-03 11:45:48 -0300

    Separando o Sidebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './Header/Header';
-import Footer from './Footer/Footer';
 import Sidebar from './Sidebar/Sidebar';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
